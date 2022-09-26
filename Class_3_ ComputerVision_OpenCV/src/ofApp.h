8e4466048611001c9553ad198d0f1c93c954c499
christinacappelli <90644921+christinacappelli@users.noreply.github.com>
2022-09-26 19:35:29 -0400

    Add files via upload

---
+++ Class_3_ ComputerVision_OpenCV/src/ofApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    int thresholdBrightness; // how bright image is is going to be the threshold
+        ofVideoGrabber grabber; // variable for main image
+        ofImage resultImg; // image of your "duplicate" -- the one you are going to be manipulating 
+		
+};
